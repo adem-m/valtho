ab7621e361b4497079f96f50e22f100e5a477fc3
Adem Mrizak <adem.mrizak@gmail.com>
2021-10-22 12:43:03 +0200

    Last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import moment from "moment";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'valtho';
+export class AppComponent implements OnInit {
+    readonly VALTHO_DATE: string = "2022-01-15T09:00:00";
+
+    months: number = 0;
+    days: number = 0;
+    hours: number = 0;
+    minutes: number = 0;
+    seconds: number = 0;
+
+    ngOnInit() {
+        this.getTimeRemaining();
+        setInterval(() => this.getTimeRemaining(), 1000);
+    }
+
+    getTimeRemaining() {
+        const total = Date.parse(this.VALTHO_DATE) - new Date().getTime();
+        this.seconds = Math.floor((total / 1000) % 60);
+        this.minutes = Math.floor((total / 1000 / 60) % 60);
+        this.hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+        this.days = Math.floor(total / (1000 * 60 * 60 * 24) % 30);
+        this.months = Math.floor(total / (1000 * 60 * 60 * 24 * 30));
+    }
+
+    isPassed(): boolean {
+        return moment(this.VALTHO_DATE).isBefore(moment());
+    }
 }
